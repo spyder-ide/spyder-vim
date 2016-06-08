75b6e877e87de4cb24aa17ae157606b949dcc615
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2016-06-08 11:45:59 +0200

    Change filename

--- spyder_vim/__init__.py
+++ spyder_vim/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 # The following statement is required to register this 3rd party plugin
-from .p_vim import Vim as PLUGIN_CLASS  # pylint: disable=C0103
+from .vim import Vim as PLUGIN_CLASS  # pylint: disable=C0103
