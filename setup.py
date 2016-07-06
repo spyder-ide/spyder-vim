79a0acefffc520e6dd5eb06344a9c562394d571b
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2016-07-06 18:12:38 +0200

    Update plugin name in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(
-    name='spyderplugins.vim',
+    name='spyder.vim',
     version="0.1.0",
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     keywords=["spyder ide plugin addon vi vim"],
